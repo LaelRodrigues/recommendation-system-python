19c8cbd5409e11a8541b7b9542b7d109239d840e
LaelRodrigues <laelrodrigues7@gmail.com>
2024-06-16 14:33:21 -0300

    att simples

--- _test.py
+++ _test.py
@@ -3,10 +3,12 @@ from RecommenderBook import load_data, recommend_books
 
 @pytest.fixture
 def loadData():
-    load_data("teste3.csv")
+    load_data("teste.csv")
 
 @pytest.mark.parametrize("user_id", ["A3UH4UZ4RSVO82"])  
 @pytest.mark.parametrize("k", [5])  
 def test_recommend_books(benchmark, loadData, user_id, k):
     result = benchmark.pedantic(recommend_books, args=(user_id, k), rounds=5)
     assert result is not None
+
+
